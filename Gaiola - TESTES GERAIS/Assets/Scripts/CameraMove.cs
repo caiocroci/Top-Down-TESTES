3e341843449e73816ea5ee9d9064738d9c77706a
caiocroci <caiocroci@gmail.com>
2020-04-02 18:22:08 -0300

    Atualização

---
+++ Gaiola - TESTES GERAIS/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+	public Transform player;
+	public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position != player.position)
+		{
+			Vector3 playerPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+			transform.position = Vector3.Lerp(transform.position, playerPosition, smoothing);
+		}
+    }
+}
---
+++ Gaiola - TESTES GERAIS/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	public Joystick joystick;
+	public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		JoystickMove();
+    }
+
+	void JoystickMove()
+	{
+		if (joystick.Horizontal >= .2f)
+		{
+			rb.velocity = (new Vector2(joystick.Horizontal * moveSpeed * Time.deltaTime, joystick.Vertical * moveSpeed * Time.deltaTime));
+		}
+		else if (joystick.Horizontal <= .2f)
+		{
+			rb.velocity = (new Vector2(joystick.Horizontal * moveSpeed * Time.deltaTime, joystick.Vertical * moveSpeed * Time.deltaTime));
+		}
+	}
+
+}
