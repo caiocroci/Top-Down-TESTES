a613a9a57e614db1a61f2ffcf9391a15a71522e6
caiocroci <caiocroci@gmail.com>
2020-04-10 19:54:26 -0300

    Atualização de animações com sprites originais

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,31 +5,47 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 	private Rigidbody2D rb;
-	public Joystick joystick;
-	public float moveSpeed = 5f;
+	public Joystick joystick; //Joystick virtual da AssetStore
+	public float moveSpeed = 5f; //Velocidade de movimento do player
+	public Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		JoystickMove();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		JoystickMove(); //função que chama a movimentação do player usando o joystick
+
+		Animation(); //função que chama as animações de movimentação do player
+	}
 
 	void JoystickMove()
 	{
-		if (joystick.Horizontal >= .2f)
+		if (joystick.Horizontal >= .2f) 
 		{
-			rb.velocity = (new Vector2(joystick.Horizontal * moveSpeed * Time.deltaTime, joystick.Vertical * moveSpeed * Time.deltaTime));
+			rb.velocity = (new Vector2(joystick.Horizontal * moveSpeed, joystick.Vertical * moveSpeed));
 		}
 		else if (joystick.Horizontal <= .2f)
 		{
-			rb.velocity = (new Vector2(joystick.Horizontal * moveSpeed * Time.deltaTime, joystick.Vertical * moveSpeed * Time.deltaTime));
+			rb.velocity = (new Vector2(joystick.Horizontal * moveSpeed, joystick.Vertical * moveSpeed));
 		}
 	}
 
+	void Animation()
+	{
+		animator.SetFloat("Horizontal", joystick.Horizontal);
+		animator.SetFloat("Vertical", joystick.Vertical);
+		if (joystick.Horizontal > 0f || joystick.Vertical > 0f)
+		{
+			animator.SetBool("Moving", true);
+		}
+		else
+		{
+			animator.SetBool("Moving", false);
+		}
+	}
 }
