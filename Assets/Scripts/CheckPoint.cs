1675e50201433a66aaf7cb7afc7480541d93d266
Jenner-JTJR <56452812+Jenner-JTJR@users.noreply.github.com>
2020-05-04 01:45:35 -0300

    HUD
    Hud colcocada no Game

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+public GameObject LastCheckPoint;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    } 
+    public void OnTriggerEnter(Collider collision2D)
+	{
+		if (collision2D.CompareTag("CheckPoint"))
+		{
+
+        }
+    }        
+}
